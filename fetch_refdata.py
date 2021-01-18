7e486d34e5ecd6a0a0989cc73247a09f8ef30f8d
Alex Tucker <alex@floop.org.uk>
2021-01-18 17:50:44 +0000

    Add attribute and unit URIs.

--- fetch_refdata.py
+++ fetch_refdata.py
@@ -3,13 +3,29 @@ import json
 
 import requests
 
-dims = [d['d']['value'] for d in
-        requests.post('https://staging.gss-data.org.uk/sparql',
-                     headers={'Accept': 'application/sparql-results+json'},
-                     data={'query': 'SELECT DISTINCT * { ?d a <http://purl.org/linked-data/cube#DimensionProperty> }'}
-                     ).json()['results']['bindings']
-        if d['d']['value'] != 'http://purl.org/linked-data/cube#measureType'
-        ]
+
+def query_results(query: str) -> list[dict]:
+    return requests.post('https://staging.gss-data.org.uk/sparql',
+                         headers={'Accept': 'application/sparql-results+json'},
+                         data={'query': query}
+                         ).json()['results']['bindings']
+
+
+dims = [
+    d['d']['value'] for d in query_results(
+        'SELECT DISTINCT * { ?d a <http://purl.org/linked-data/cube#DimensionProperty> }')
+    if d['d']['value'] != 'http://purl.org/linked-data/cube#measureType'
+]
+
+attrs = [
+    a['a']['value'] for a in query_results(
+        'SELECT DISTINCT * { ?a a <http://purl.org/linked-data/cube#AttributeProperty> }')
+]
+
+units = [
+    u['u']['value'] for u in query_results(
+        'SELECT DISTINCT * { [] <http://purl.org/linked-data/sdmx/2009/attribute#unitMeasure> ?u }')
+]
 
 with open('external.json', 'r') as ext_file:
     ext_defs = json.load(ext_file)
@@ -19,6 +35,20 @@ ext_defs['definitions']['dim_ext_uri'] = {
     "type": "string",
     "format": "uri",
     "enum": dims
+}
+
+ext_defs['definitions']['attr_ext_uri'] = {
+    "$id": "#attr_ext_uri",
+    "type": "string",
+    "format": "uri",
+    "enum": attrs
+}
+
+ext_defs['definitions']['unit_ext_uri'] = {
+    "$id": "#attr_ext_uri",
+    "type": "string",
+    "format": "uri",
+    "enum": units
 }
 
 with open('external.json', 'w') as ext_file:
