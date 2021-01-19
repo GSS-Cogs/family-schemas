efb37d61eaeb5bb63e6086bef060960fc4341936
Alex Tucker <alex@floop.org.uk>
2021-01-19 11:03:40 +0000

    Add measures and concept scheme URIs.

--- fetch_refdata.py
+++ fetch_refdata.py
@@ -5,50 +5,57 @@ import requests
 
 
 def query_results(query: str) -> list[dict]:
-    return requests.post('https://staging.gss-data.org.uk/sparql',
-                         headers={'Accept': 'application/sparql-results+json'},
-                         data={'query': query}
-                         ).json()['results']['bindings']
+    return [
+        res['v']['value'] for res in requests.post(
+            'https://staging.gss-data.org.uk/sparql',
+            headers={'Accept': 'application/sparql-results+json'},
+            data={'query': query}
+        ).json()['results']['bindings']
+    ]
 
-
-dims = [
-    d['d']['value'] for d in query_results(
-        'SELECT DISTINCT * { ?d a <http://purl.org/linked-data/cube#DimensionProperty> }')
-    if d['d']['value'] != 'http://purl.org/linked-data/cube#measureType'
-]
-
-attrs = [
-    a['a']['value'] for a in query_results(
-        'SELECT DISTINCT * { ?a a <http://purl.org/linked-data/cube#AttributeProperty> }')
-]
-
-units = [
-    u['u']['value'] for u in query_results(
-        'SELECT DISTINCT * { [] <http://purl.org/linked-data/sdmx/2009/attribute#unitMeasure> ?u }')
-]
 
 with open('external.json', 'r') as ext_file:
     ext_defs = json.load(ext_file)
 
-ext_defs['definitions']['dim_ext_uri'] = {
-    "$id": "#dim_ext_uri",
-    "type": "string",
-    "format": "uri",
-    "enum": dims
-}
-
-ext_defs['definitions']['attr_ext_uri'] = {
-    "$id": "#attr_ext_uri",
-    "type": "string",
-    "format": "uri",
-    "enum": attrs
-}
-
-ext_defs['definitions']['unit_ext_uri'] = {
-    "$id": "#attr_ext_uri",
-    "type": "string",
-    "format": "uri",
-    "enum": units
+ext_defs['definitions'] = {
+    "dim_ext_uri": {
+        "$id": "#dim_ext_uri",
+        "type": "string",
+        "format": "uri",
+        "enum": [
+            d for d in query_results(
+                'SELECT DISTINCT * { ?v a <http://purl.org/linked-data/cube#DimensionProperty> }')
+            if d != 'http://purl.org/linked-data/cube#measureType'
+        ]
+    },
+    "attr_ext_uri": {
+        "$id": "#attr_ext_uri",
+        "type": "string",
+        "format": "uri",
+        "enum": query_results(
+            'SELECT DISTINCT * { ?v a <http://purl.org/linked-data/cube#AttributeProperty> }')
+    },
+    "unit_ext_uri": {
+        "$id": "#unit_ext_uri",
+        "type": "string",
+        "format": "uri",
+        "enum": query_results(
+            'SELECT DISTINCT * { [] <http://purl.org/linked-data/sdmx/2009/attribute#unitMeasure> ?v }')
+    },
+    "meas_ext_uri": {
+        "$id": "#meas_ext_uri",
+        "type": "string",
+        "format": "uri",
+        "enum": query_results(
+            'SELECT DISTINCT * { [] <http://purl.org/linked-data/cube#measureType> ?v }')
+    },
+    "cs_ext_uri": {
+        "$id": "#cs_ext_uri",
+        "type": "string",
+        "format": "uri",
+        "enum": query_results(
+            'SELECT DISTINCT * { ?v a <http://www.w3.org/2004/02/skos/core#ConceptScheme> }')
+    }
 }
 
 with open('external.json', 'w') as ext_file:
